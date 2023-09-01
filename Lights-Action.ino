4e2b23c359cd59a81dc001a5adca416bf7b5c851
Shroomicus <linvincent969@gmail.com>
2023-08-31 22:01:30 -0400

    Add change of style of lights, brightness, and multiple styles/modes

--- Lights-Action.ino
+++ Lights-Action.ino
@@ -1,14 +1,11 @@
 #include <FastLED.h>
+#include <math.h>
 
 #define LED_PIN     10
 #define NUM_LEDS    100
 
 CRGB leds[NUM_LEDS];
-
-void setup() {
-    Serial.begin(9600);
-    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-}
+int lines[3];
 
 CRGB color = 500394;
 
@@ -16,6 +13,16 @@ char currChar;
 char message[64];
 int mPoint = 0;
 boolean notTerm = true;
+int mode = 1;
+
+void setup() {
+    lines[0] = 29;
+    lines[1] = 33;
+    lines[2] = 16 + 22;
+    Serial.begin(9600);
+    FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(100);
+}
 
 void loop() {
     if(Serial.available()){
@@ -25,15 +32,69 @@ void loop() {
         Serial.println(message);
         unsigned long int col = 0;
         if(message[0] == 'C'){
+            col = 0;
             for(int i = 1; message[i] != 0; i++){
                 // Serial.println(col);
                 col = (col * 10) + (message[i] - '0');
             }
+            color = col;
+        }
+        else if(message[0] == 'B'){
+            int bright = 0;
+            for(int i = 1; message[i] != 0; i++){
+                bright = (bright * 10) + (message[i] - '0');
+            }
+            FastLED.setBrightness(bright);
+        }
+        else if(message[0] == 'M'){
+            mode = 0;
+            for(int i = 1; message[i] != 0; i++){
+                mode = (mode * 10) + (message[i] - '0');
+            }
+            Serial.println(mode);
+        }
+        for(int i = 0; i<64; i++){
+            message[i] = 0;
         }
     }
-    for(int i = 0; i<NUM_LEDS; i++){
-        leds[i] = color;
+
+
+
+    if(mode == 0){
+        for(int i = 0; i<NUM_LEDS; i++){
+            leds[i] = color;
+        }
+    }
+    if(mode == 1){
+        fadeToBlackBy(leds, NUM_LEDS, 20);
+        for(int i = 0; i < 3; i++){
+          int prev = 0;
+          for(int j = 0; j < i; j++){
+            prev += lines[j];
+          }
+          int pos = 0;
+          if(i % 2 == 0){
+            pos += abs(beat8(16) - 256.0/2) / (256.0/2) * lines[i] + prev;
+          }
+          else {
+            pos += lines[i] - (abs(beat8(16) - 256.0/2) / (256.0/2) * lines[i]) + prev;
+          }
+          leds[pos] += color;
+        }
+    }
+    if(mode == 2){
+        fadeToBlackBy(leds, NUM_LEDS, 20);
+        for(int i = 0; i < 3; i++){
+          int prev = 0;
+          for(int j = 0; j < i; j++){
+            prev += lines[j];
+          }
+          int pos = 0;
+          pos = abs(beat8(16) - 256.0/2) / (256.0/2) * lines[i] + prev;\
+          leds[pos] += color;
+          pos = lines[i] - (abs(beat8(16) - 256.0/2) / (256.0/2) * lines[i]) + prev;
+          leds[pos] += color;
+        }
     }
     FastLED.show();
-
 }
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import time
 
 def main():
     arduino = serial.Serial('COM7', 9600, timeout = 1)
-    hex = "C" + str(int("940303", 16))
-    print(bytes(hex, 'utf-8'))
-    arduino.write(bytes(hex, 'utf-8'))
+    
     while(True):
+        inp = input()
+        if(inp[0] == "C"):
+            inp = "C" + str(int(inp[1:], 16))
+        # print(bytes(hex, 'utf-8'))
+        arduino.write(bytes(inp, 'utf-8'))
         print(arduino.readline())
 if __name__ == "__main__":
     main()
